a4c4d06217e1d0b6f5d9c933f896123cd6372872
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-05-14 22:50:19 +0300

    add place bid function

---
+++ auction-service/src/handlers/placeBid.js
@@ -0,0 +1,39 @@
+import AWS from "aws-sdk";
+import createError from "http-errors";
+
+import commonMiddleware from "../lib/commonMiddleware";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id },
+    UpdateExpression: "set highestBid.amount = :amount",
+    ExpressionAttributeValues: {
+      ":amount": amount,
+    },
+    ReturnValues: "ALL_NEW",
+  };
+
+  let updatedAuction;
+
+  try {
+    const result = await dynamodb.update(params).promise();
+
+    updatedAuction = result.Attributes;
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
+}
+
+export const handler = commonMiddleware(placeBid);
