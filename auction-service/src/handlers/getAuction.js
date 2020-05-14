1d12d2b3b62712ce63c10ae646f4a5d8533f077b
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-05-14 22:26:04 +0300

    add get auctions/auction by id functions

---
+++ auction-service/src/handlers/getAuction.js
@@ -0,0 +1,41 @@
+import AWS from "aws-sdk";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuction(event, context) {
+  const { id } = event.pathParameters;
+  let auction;
+
+  try {
+    const result = await dynamodb
+      .get({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Key: { id },
+      })
+      .promise();
+
+    auction = result.Item;
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  if (!auction) {
+    throw new createError.NotFound(`Auction with ID ${id} doesn't exist.`);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = middy(getAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
