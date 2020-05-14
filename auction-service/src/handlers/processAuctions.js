81f49766c063d938ef82f997ed151c482e44c694
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-05-15 01:36:49 +0300

    add closing auction func.

--- auction-service/src/handlers/processAuctions.js
+++ auction-service/src/handlers/processAuctions.js
@@ -1,5 +1,21 @@
+import createError from "http-errors";
+
+import { getEndedAuctions } from "../lib/getEndedAuctions";
+import { closeAuction } from "../lib/closeAuction";
+
 async function processAuctions(event, context) {
-  console.log("processing auctions");
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map((auction) =>
+      closeAuction(auction)
+    );
+
+    await Promise.all(closePromises);
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
